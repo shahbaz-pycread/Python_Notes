ee9010a53ff577216d642d2054bf6f40d4dca7a0
shahbaz-pycread <alam.shahbaz01@gmail.com>
2020-10-05 16:32:44 +0530

    Added 'dictionaries'

---
+++ dictionaries/dictionary.py
@@ -0,0 +1,20 @@
+print(
+'''
+    A dictionary is a collection which is unordered,
+    changeable and indexed. In Python, dictionaries are
+    written with curly brackets, and they have
+    keys and values.
+    ---------------------------------------------------
+
+'''
+)
+
+# Create and print a dictionary
+
+csk_team = {
+    'Captain' : 'MS Dhoni',
+    'Coach' : 'Stephen Fleming',
+    'cup_last_won' : 2018
+}
+# print(f'The Captain of the team is {csk_team['Captain']}')
+print('The captain of the team is ' + csk_team['Captain'])
